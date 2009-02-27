71bcf6bbd52aec10a3297a7add30ea006fc84b2a
blindgaenger <blindgaenger@gmail.com>
2009-02-28 00:38:07 +0100

    added model support for DataMapper and ActiveRecord

--- lib/rest.rb
+++ lib/rest.rb
@@ -18,6 +18,22 @@ module Stone
   end
 end
 
+module DataMapper
+  module Model
+    def find_by_id(id)
+      get(id)
+    end
+  end
+end
+
+module ActiveRecord
+  module Base
+    def find_by_id(id)
+      first(id)
+    end
+  end
+end
+
 module Sinatra
   module REST
 
